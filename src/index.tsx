1cb6c2c1a9c0b4d2a5ea96990362970618186dd2
Vasily Sibirev <vasily.sib@gmail.com>
2023-02-26 19:49:58 +0500

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,5 @@
+import 'core-js/stable'
+import 'regenerator-runtime/runtime'
+import 'simplebar-react/dist/simplebar.min.css'
+
+console.log('check!', document.currentScript)
---
+++ webpack.config.ts
@@ -0,0 +1,84 @@
+import { WebpackConfiguration } from 'webpack-dev-server'
+import HtmlWebpackPlugin from 'html-webpack-plugin'
+import path from 'path'
+
+import { supportedLngs } from './src/i18n'
+
+export default (env: IEnvironment, args: IWebpackArgs): WebpackConfiguration => {
+  const serve = env.WEBPACK_SERVE == true
+  const build = env.WEBPACK_BUILD == true
+  const production = args.mode == 'production'
+
+  return {
+    mode: production ? 'production' : 'development',
+    devtool: production ? undefined : 'eval-source-map',
+    
+    devServer: serve ? {
+      hot: true,
+      port: 8080,
+      static: path.resolve(__dirname, 'public')
+    } : undefined,
+
+    entry: { 'salty-cube': './src/index.tsx' },
+    output: build ? {
+      clean: true,
+      path: path.resolve(__dirname, 'build')
+    } : undefined,
+
+    resolve: {
+      extensions: [ '.js', '.ts', '.tsx' ]
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                [ '@babel/preset-env', { targets: "defaults", useBuiltIns: 'entry', corejs: '3.28' } ],
+                '@babel/preset-react',
+                '@babel/preset-typescript'
+              ],
+              plugins: [
+                'babel-plugin-styled-components',
+                [
+                  'i18next-extract',
+                  {
+                    outputPath: 'src/i18n/resources/{{locale}}/{{ns}}.json',
+                    locales: supportedLngs,
+                    useI18nextDefaultValue: true,
+                    keyAsDefaultValue: true
+                  }
+                ]
+              ]
+            }
+          }
+        },
+        {
+          test: /\.css$/,
+          use: [ 'style-loader', 'css-loader' ]
+        }
+      ]
+    },
+
+    plugins: serve ? undefined : [
+      new HtmlWebpackPlugin({
+        template: 'public/index.html',
+        inject: false
+      })
+    ]
+  }
+}
+
+interface IEnvironment {
+  WEBPACK_SERVE?: boolean
+  WEBPACK_BUILD?: boolean
+}
+
+interface IWebpackArgs {
+  mode?: 'production' | 'development'
+  env: IEnvironment
+}
