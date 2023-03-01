ebdaa09f2fbd57366244b901ea096e089aa05978
Vasily Sibirev <vasily.sib@gmail.com>
2023-03-01 19:52:45 +0500

    missing export

--- src/utils/salty-cube-provider/index.ts
+++ src/utils/salty-cube-provider/index.ts
@@ -1,2 +1,3 @@
 export { SaltyCubeProvider, useSaltyCube } from './salty-cube-provider'
 export type { ISaltyCubeProviderProps, ISaltyCubeContext } from './types'
+export type { SaltyCubeAction } from './actions'
